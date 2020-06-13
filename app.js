ac476e04fb7dcbfb2286b5ca9e3fcf43fc1b59ce
Daniel Mermelstein <danmermel@gmail.com>
2020-06-13 08:49:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const solve = async (clue, clueType) => {
+  var obj = { clue: clue }
+  var response = await fetch('https://prod.remebit.com/' + clueType, {
+     method: 'post',
+     body: JSON.stringify(obj)
+  })
+  var solution =  await response.json()
+  return solution
+}
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    clue: '',
+    progress: 0,
+    solving: false,
+    solutions: [],
+    anagram: '',
+    anagramSolutions: []
+  },
+
+  methods: {
+    solveAnagram: async function () {
+      this.anagramSolutions = await solve(this.anagram, 'solver')
+    },
+
+    analyze: async function () {
+      const self = this
+      self.solutions = []
+      self.solving = true
+      self.progress = 0
+      const increment = 100/8
+
+     const clueTypes = ["anagram", "hiddenwords", "containers", "reversals", "doubledef", "charades", "homophones", "subtractions"]
+
+     for (var i in clueTypes) {
+       var clueType = clueTypes[i]
+       solve(this.clue, clueType).then(function(solutions) {
+         self.solutions = self.solutions.concat(solutions)
+         self.progress += increment
+       }).catch(function(err) {
+         self.progress += increment
+         console.log (clueType, " err ", err)
+       })
+     }  //end for
+   } // analyze
+  } //methods
+})
